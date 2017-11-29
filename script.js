fc8151310422cee0436b812fc3b46afab8546001
Lauri <lauri.voipio@aalto.fi>
2017-11-29 13:56:16 +0200

    Toimiikohan edelleen?

---
+++ script.js
@@ -0,0 +1,29 @@
+<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyALTOc8Xejm7fGqvlxju2JVLc2YAcHHArE",
+    authDomain: "crowdsourcedtranslation-696a0.firebaseapp.com",
+    databaseURL: "https://crowdsourcedtranslation-696a0.firebaseio.com",
+    projectId: "crowdsourcedtranslation-696a0",
+    storageBucket: "crowdsourcedtranslation-696a0.appspot.com",
+    messagingSenderId: "445544329993"
+  };
+  firebase.initializeApp(config);
+
+var messagesRef = new Firebase('https://crowdsourcedtranslation-696a0.firebaseio.com');
+var messageField = document.getElementById('messageInput');
+
+// Save data to firebase
+function savedata(){
+  var sourceLanguage = $('post-sourclang').text();
+  var targetLanguage = $('post-targlang').text();
+  var transtext = $('post-text').text();
+  var transtags = $('post-tags').text();
+
+  firebaseRef.set({Source: sourceLanguage, Target: targetLanguage, Ttext:transtext, Tags:transtags});
+  evt.preventDefault();
+}
+var postRef = firebaseRef.push();
+submit.onclick = postRef
+</script>
