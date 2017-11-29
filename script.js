c59090f2e4a77601d195c908670a58d168ab71ea
Lauri <lauri.voipio@aalto.fi>
2017-11-29 14:23:55 +0200

    katotaa

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 <script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
 <script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyALTOc8Xejm7fGqvlxju2JVLc2YAcHHArE",
-    authDomain: "crowdsourcedtranslation-696a0.firebaseapp.com",
-    databaseURL: "https://crowdsourcedtranslation-696a0.firebaseio.com",
-    projectId: "crowdsourcedtranslation-696a0",
-    storageBucket: "crowdsourcedtranslation-696a0.appspot.com",
-    messagingSenderId: "445544329993"
-  };
-  //firebase.initializeApp(config);
+  // // Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyALTOc8Xejm7fGqvlxju2JVLc2YAcHHArE",
+  //   authDomain: "crowdsourcedtranslation-696a0.firebaseapp.com",
+  //   databaseURL: "https://crowdsourcedtranslation-696a0.firebaseio.com",
+  //   projectId: "crowdsourcedtranslation-696a0",
+  //   storageBucket: "crowdsourcedtranslation-696a0.appspot.com",
+  //   messagingSenderId: "445544329993"
+  // };
+  // //firebase.initializeApp(config);
 
   var firebaseRef = new Firebase('https://crowdsourcedtranslation-696a0.firebaseio.com');
 
